ae0bb3fa3817fe5c14bb3fa321ef4a7b5c98f98a
Георгий Александрович Мичкасов <michkasov.ga1668@oksei.onmicrosoft.com>
2023-04-29 11:55:11 +0500

    Добавьте файлы проекта.

---
+++ Wallet/Article.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wallet
+{
+    [Serializable]
+    public class Articles
+    {
+        public List<Article> ArticleList { get; set; } = new List<Article>();
+    }
+
+    [Serializable]
+    public class Article
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date_create { get; set; }
+
+
+        public Article()
+        {
+
+        }
+
+        public Article(string name, string description, DateTime date_create)
+        {
+            Name = name;
+            Description = description;
+            Date_create = date_create;
+        }
+    }
+}
---
+++ Wallet/CreateArticle.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace Wallet
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateArticle.xaml
+    /// </summary>
+    public partial class CreateArticle : Window
+    {
+
+        public ListView ListViewArticles { get; set; }
+
+        public CreateArticle(ListView listViewArticles)
+        {
+            InitializeComponent();
+            ListViewArticles = listViewArticles;
+        }
+
+      
+        private void Click_CreateArticle(object sender, RoutedEventArgs e)
+        {
+            Article article = new Article(NameTextBox.Text,"" , DateTime.Now);
+
+            ListViewItem LVI = new ListViewItem();
+            LVI.Tag = article;
+            LVI.Content = article.Name;
+
+            ListViewArticles.Items.Add(LVI);
+
+            SerializableDate();
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.update(ListViewArticles);
+            Close();
+        }
+
+        private void SerializableDate()
+        {
+            Articles articles = new Articles();
+            foreach (ListViewItem item in ListViewArticles.Items)
+            {
+                articles.ArticleList.Add((Article)item.Tag);
+            }
+
+            SerializableInXML(articles);
+        }
+
+        private void SerializableInXML(Articles articles)
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(Articles));
+
+            using (FileStream fs = new FileStream("Articles.xml", FileMode.OpenOrCreate))
+            {
+                xml.Serialize(fs, articles);
+            }
+        }
+
+        private void Click_CloseWindow(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
