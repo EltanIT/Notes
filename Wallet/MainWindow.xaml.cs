90661127b1d85e9f92454b9ecbb518120f09930e
Георгий Александрович Мичкасов <michkasov.ga1668@oksei.onmicrosoft.com>
2023-04-29 12:04:02 +0500

    save and read)

--- Wallet/MainWindow.xaml.cs
+++ Wallet/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +13,36 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
 
 namespace Wallet
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            Articles articles = DeserializiXML();
+
+            foreach (Article item in articles.ArticleList)
+            {
+                ListViewItem LVI = new ListViewItem();
+                LVI.Tag = item;
+                LVI.Content = item.Name;
+
+                ListViewArticles.Items.Add(LVI);
+            }
+        }
+
+        private Articles DeserializiXML()
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(Articles));
+
+            using (FileStream fs = new FileStream("Articles.xml", FileMode.OpenOrCreate))
+            {
+               return (Articles)xml.Deserialize(fs);
+            }
         }
 
         public void update(ListView listViewArticles)
